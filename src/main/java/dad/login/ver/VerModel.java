d7eb993a359615f1e02d147aed92271ca3f0efe2
AdrianNietoRamos <adrianietoemfb@gmail.com>
2021-10-21 13:14:49 +0100

    Update VerModel.java

--- src/main/java/dad/login/ver/VerModel.java
+++ src/main/java/dad/login/ver/VerModel.java
@@ -1,5 +1,13 @@
 package dad.login.ver;
 
-public class VerModel {
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
 
+public class VerModel {
+private final StringProperty usuario=new SimpleStringProperty();
+
+public final StringProperty usuarioProperty() {
+	return this.usuario;
+	
+}
 }
