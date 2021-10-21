4ac319f5434e53e5508ed19e4b610c18cf59a166
AdrianNietoRamos <adrianietoemfb@gmail.com>
2021-10-21 13:23:38 +0100

    Update VerModel.java

--- src/main/java/dad/login/ver/VerModel.java
+++ src/main/java/dad/login/ver/VerModel.java
@@ -10,6 +10,20 @@ public class VerModel {
 		usuario=user;
 		this.contrasena=password;
 	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getContrasena() {
+		return contrasena;
+	}
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+	
+	
 
 	
 }
